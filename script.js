8594459e372026f4316e4ff6ae2aa6f95a685031
22-shivam <54504024+shivam1549@users.noreply.github.com>
2024-05-08 08:23:05 +0530

    Fix Save submit button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const questions = [
         "op2": "Hypertext Programming",
         "op3": "Personal Home Page",
         "op4": "Programming Hypertext Processor",
+        "answer": "op1",
         "state": null,
         "selectedans": null
     },
@@ -16,6 +17,7 @@ const questions = [
         "op2": "Python",
         "op3": "Java",
         "op4": "C++",
+        "answer": "op2",
         "state": null,
         "selectedans": null
     },
@@ -26,6 +28,7 @@ const questions = [
         "op2": "Hyper Textual Markup Language",
         "op3": "High Text Markup Language",
         "op4": "Hyperlink Text Markup Language",
+        "answer": "op3",
         "state": null,
         "selectedans": null
     },
@@ -36,6 +39,7 @@ const questions = [
         "op2": "HTML",
         "op3": "Ruby",
         "op4": "C++",
+        "answer": "op2",
         "state": null,
         "selectedans": null
     },
@@ -46,6 +50,7 @@ const questions = [
         "op2": "Berlin",
         "op3": "Madrid",
         "op4": "Paris",
+        "answer": "op4",
         "state": null,
         "selectedans": null
     },
@@ -56,6 +61,7 @@ const questions = [
         "op2": "Angular",
         "op3": "Bootstrap",
         "op4": "Node.js",
+        "answer": "op3",
         "state": null,
         "selectedans": null
     },
@@ -66,6 +72,7 @@ const questions = [
         "op2": "Bill Gates",
         "op3": "Elon Musk",
         "op4": "Mark Zuckerberg",
+        "answer": "op3",
         "state": null,
         "selectedans": null
     },
@@ -76,6 +83,7 @@ const questions = [
         "op2": "2000",
         "op3": "2005",
         "op4": "2010",
+        "answer": "op1",
         "state": null,
         "selectedans": null
     },
@@ -86,6 +94,7 @@ const questions = [
         "op2": "O2",
         "op3": "H2O",
         "op4": "HO",
+        "answer": "op3",
         "state": null,
         "selectedans": null
     },
@@ -96,6 +105,7 @@ const questions = [
         "op2": "Mars",
         "op3": "Jupiter",
         "op4": "Saturn",
+        "answer": "op2",
         "state": null,
         "selectedans": null
     }
@@ -103,134 +113,182 @@ const questions = [
 
 // console.log(questions[0])
 
-function loadquestions(i){
-    var html ="";
+function loadquestions(i) {
+    var html = "";
     // alert(i)
     // console.log(questions[i])
-// questions.forEach(element => {
-    html += "<div class='card quest-card' data-questid='"+questions[i].id+"'>"
-   
+    // questions.forEach(element => {
+
+    if (i >= questions.length) {
+        var markers = '';
+        var quesindex = 0
+        markers += "<div class='row'>"
+        questions.forEach(element => {
+            markers += "<div class='col-md-2 mb-2'>"
+            markers += "<div data-questindex='" + quesindex + "' " + (element.state && element.state == 'answered' ? 'style="background-color:green"' : '') + " " + (element.state && element.state == 'unanswered' ? 'style="background-color:red"' : '') + " " + (element.state && element.state == 'markedreview' ? 'style="background-color:purple"' : '') + " class='border text-center p-2 navquestions'>" + element.id + "</div>"
+            markers += "</div>"
+            quesindex++;
+        });
+        markers += "</div>"
+        document.querySelector("#markers").innerHTML = markers;
+        return false;
+    }
+
+    html += "<div class='card quest-card' data-questid='" + questions[i].id + "'>"
+
     html += "<div class='card-body'>"
-    html += "<p class='card-title'>"+questions[i].quest+"</p>"
-    html += "<div class='form-check'><input "+( questions[i].selectedans && questions[i].selectedans == questions[i].op1 ? 'checked' : '' )+" data-questid='"+questions[i].id+"' value='"+questions[i].op1+"' type='radio' class='form-check-input radioopt'><label>" +questions[i].op1+ "</label></div>"
-    html += "<div class='form-check'><input "+( questions[i].selectedans && questions[i].selectedans == questions[i].op2 ? 'checked' : '' )+" data-questid='"+questions[i].id+"' value='"+questions[i].op2+"' type='radio' class='form-check-input radioopt'><label>" +questions[i].op2+ "</label></div>"
-    html += "<div class='form-check'><input "+( questions[i].selectedans && questions[i].selectedans == questions[i].op3 ? 'checked' : '' )+" data-questid='"+questions[i].id+"' value='"+questions[i].op3+"' type='radio' class='form-check-input radioopt'><label>" +questions[i].op3+ "</label></div>"
-    html += "<div class='form-check'><input "+( questions[i].selectedans && questions[i].selectedans == questions[i].op4 ? 'checked' : '' )+" data-questid='"+questions[i].id+"' value='"+questions[i].op4+"' type='radio' class='form-check-input radioopt'><label>" +questions[i].op4+ "</label></div>"
+    html += "<p class='card-title'>(" + questions[i].id + ") " + questions[i].quest + "</p>"
+    html += "<div class='form-check'><input " + (questions[i].selectedans && questions[i].selectedans == 'op1' ? 'checked' : '') + " data-questid='" + questions[i].id + "' value='op1' type='radio' class='form-check-input radioopt'><label>" + questions[i].op1 + "</label></div>"
+    html += "<div class='form-check'><input " + (questions[i].selectedans && questions[i].selectedans == 'op2' ? 'checked' : '') + " data-questid='" + questions[i].id + "' value='op2' type='radio' class='form-check-input radioopt'><label>" + questions[i].op2 + "</label></div>"
+    html += "<div class='form-check'><input " + (questions[i].selectedans && questions[i].selectedans == 'op3' ? 'checked' : '') + " data-questid='" + questions[i].id + "' value='op3' type='radio' class='form-check-input radioopt'><label>" + questions[i].op3 + "</label></div>"
+    html += "<div class='form-check'><input " + (questions[i].selectedans && questions[i].selectedans == 'op4' ? 'checked' : '') + " data-questid='" + questions[i].id + "' value='op4' type='radio' class='form-check-input radioopt'><label>" + questions[i].op4 + "</label></div>"
     html += "</div></div>"
-// });
-document.querySelector("#container").innerHTML = html;
-if(i === 0){
-    document.querySelector("#previous").style.display = "none";  
-}
-else{
-    document.querySelector("#previous").style.display = "inline-block";   
-}
-if(i != questions.length - 1){
-    document.querySelector("#next").style.display = "inline-block";   
-    document.querySelector("#marknext").style.display = "inline-block";
-    document.querySelector("#savenext").style.display = "inline-block";
-}
-else{
-    document.querySelector("#next").style.display = "none";
-    document.querySelector("#marknext").style.display = "none";
-    document.querySelector("#savenext").style.display = "none";
-    
-    
-}
 
-i == questions.length - 1 ? document.querySelector("#submittest").style.display = "inline-block" : document.querySelector("#submittest").style.display = "none";
-// load markers
-var markers = '';
-var quesindex = 0
-markers += "<div class='row'>"
-questions.forEach(element => {
-markers += "<div class='col-md-2 mb-2'>"
-markers += "<div data-questindex='"+quesindex+"' "+( element.state && element.state == 'answered' ? 'style="background-color:green"': '' )+" "+(element.state && element.state == 'unanswered' ? 'style="background-color:red"': '' )+" "+(element.state && element.state == 'markedreview' ? 'style="background-color:purple"': '' )+" class='border text-center p-2 navquestions'>"+element.id+"</div>"
-markers += "</div>"
-quesindex++;
-});
-markers += "</div>"
-document.querySelector("#markers").innerHTML = markers;
+    // });
+    document.querySelector("#container").innerHTML = html;
+    if (i === 0) {
+        document.querySelector("#previous").style.display = "none";
+    }
+    else {
+        document.querySelector("#previous").style.display = "inline-block";
+    }
+    if (i != questions.length - 1) {
+        document.querySelector("#next").style.display = "inline-block";
+        document.querySelector("#marknext").style.display = "inline-block";
+        document.querySelector("#savenext").style.display = "inline-block";
+    }
+    else {
+        document.querySelector("#next").style.display = "none";
+        document.querySelector("#marknext").style.display = "none";
+        // document.querySelector("#savenext").innerHTML = "Save";
 
 
-var navqustions = document.querySelectorAll(".navquestions");
-navqustions.forEach(function(element){
-    
-    element.addEventListener("click", function(){
-       var questionindex = this.getAttribute("data-questindex");
-       console.log(questionindex)
-       loadquestions(questionindex);
+    }
+
+
+    // load markers
+    var markers = '';
+    var quesindex = 0
+    markers += "<div class='row'>"
+    questions.forEach(element => {
+        markers += "<div class='col-md-2 mb-2'>"
+        markers += "<div data-questindex='" + quesindex + "' " + (element.state && element.state == 'answered' ? 'style="background-color:green"' : '') + " " + (element.state && element.state == 'unanswered' ? 'style="background-color:red"' : '') + " " + (element.state && element.state == 'markedreview' ? 'style="background-color:purple"' : '') + " class='border text-center p-2 navquestions'>" + element.id + "</div>"
+        markers += "</div>"
+        quesindex++;
+    });
+    markers += "</div>"
+    document.querySelector("#markers").innerHTML = markers;
+
+
+    var navqustions = document.querySelectorAll(".navquestions");
+    navqustions.forEach(function (element) {
+        questions[i].state == null ? questions[i].state = "unanswered" : '';
+        element.addEventListener("click", function () {
+            var questionindex = this.getAttribute("data-questindex");
+            //    console.log(questionindex)
+            // i = questionindex;
+            noquest = parseInt(questionindex);
+            //    console.log(noquest + "hfh")
+            loadquestions(questionindex);
+        })
     })
-})
 }
 var noquest = 0;
 loadquestions(noquest)
 
 // Next questions
-document.querySelector("#next").addEventListener("click", function(){
+document.querySelector("#next").addEventListener("click", function () {
     // alert("dd")
     var questid = document.querySelector(".quest-card").getAttribute("data-questid");
     // console.log(questid);
     const questionsidindex = questions.findIndex(question => question.id == parseInt(questid));
     console.log(questions[questionsidindex].state);
-    
-    questions[questionsidindex].state == null ?  questions[questionsidindex].state = "unanswered" : ''; 
+
+    questions[questionsidindex].state == null ? questions[questionsidindex].state = "unanswered" : '';
     // questions[questionsidindex].state == "markedreview" ?  "" : '';
     noquest = noquest + 1;
-    // console.log(noquest)
+    // console.log(noquest + "net")
     loadquestions(noquest);
 })
 // previos questions
-document.querySelector("#previous").addEventListener("click", function(){
+document.querySelector("#previous").addEventListener("click", function () {
     // alert("dd")
     var questid = document.querySelector(".quest-card").getAttribute("data-questid");
     console.log(questid);
     const questionsidindex = questions.findIndex(question => question.id == parseInt(questid));
-    questions[questionsidindex].state == null ?  questions[questionsidindex].state = "unanswered" : ''; 
+    questions[questionsidindex].state == null ? questions[questionsidindex].state = "unanswered" : '';
     noquest = noquest - 1;
     // console.log(noquest)
     loadquestions(noquest);
 })
 // Save qustion and next
-document.querySelector("#savenext").addEventListener("click", function(){
+document.querySelector("#savenext").addEventListener("click", function () {
     // alert("he")
-    var checkedValue ='';
+    var checkedValue = '';
     var questid = '';
     document.querySelectorAll(".radioopt").forEach(element => {
-            // console.log(element.value)
-            if(element.checked){
-                checkedValue  = element.value;
-                questid = element.getAttribute("data-questid");
-            }
-          
+        // console.log(element.value)
+        if (element.checked) {
+            checkedValue = element.value;
+            questid = element.getAttribute("data-questid");
+        }
+
     })
-    if(checkedValue === ''){
+    if (checkedValue === '') {
         alert("please select at least one value");
         return false;
-       
+
     }
     const questionsidindex = questions.findIndex(question => question.id == parseInt(questid));
     // console.log(questionsid);
     questions[questionsidindex].selectedans = checkedValue;
-    questions[questionsidindex].state = "answered"; 
+    questions[questionsidindex].state = "answered";
     console.log(questions);
     // console.log(questid);
     noquest = noquest + 1;
+
     // console.log(noquest)
     loadquestions(noquest);
 
 })
 
-document.querySelector("#marknext").addEventListener("click", function(){
+document.querySelector("#marknext").addEventListener("click", function () {
     var questid = document.querySelector(".quest-card").getAttribute("data-questid");
     console.log(questid);
     const questionsidindex = questions.findIndex(question => question.id == parseInt(questid));
-    questions[questionsidindex].state = "markedreview"; 
+    questions[questionsidindex].state = "markedreview";
     noquest = noquest + 1;
     // console.log(noquest)
     loadquestions(noquest);
     console.log(questions);
 })
+
+
+document.querySelector("#submittest").addEventListener("click", function () {
+    document.querySelector("#container").innerHTML = '';
+    document.querySelector("#markers").innerHTML = '';
+    document.querySelector("#questionconatiner").innerHTML = '';
+
+
+    const getmarks = calculateMarks();
+    const totalmarks = questions.length;
+
+    var marks = "<h2 class='text-center'><span style='color:green'>" + getmarks + "</span>Out of" + totalmarks + "</h2>";
+
+    document.querySelector("#obtmarks").innerHTML = marks;
+
+
+})
+
+function calculateMarks() {
+    var marks = 0;
+    questions.forEach(element => {
+        if (element.answer === element.selectedans) {
+            marks++;
+        }
+    })
+    return marks;
+}
 
 // Navigation question load
 
